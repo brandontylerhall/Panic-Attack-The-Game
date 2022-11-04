b5e5bc494948b7674c8207865a7c111e4cf3d0f8
Brandon Hall <bran.tyler.hall@gmail.com>
2022-11-03 21:35:41 -0400

    initial commit

---
+++ game/rooms.js
@@ -0,0 +1,38 @@
+const rooms = [
+    {
+        name: 'kitchen',
+        item: 'punch bowl',
+        npcs: 'spin-the-bottle',
+        state: 'unlocked'
+    },
+    {
+        name: 'bathroom-locked',
+        item: 'punch bowl',
+        npcs: 'cokeheads',
+        state: 'locked',
+    },
+    {
+        name: 'bathroom-unlocked',
+        item: 'punch bowl',
+        npcs: 'cokeheads-dead',
+        state: 'unlocked'
+    },
+    {
+        name: 'study',
+        item: 'punch bowl',
+        npcs: 'spin-the-bottle',
+        state: 'unlocked'
+    },
+    {
+        name: 'stairs-locked',
+        item: 'punch bowl',
+        npcs: 'spin-the-bottle',
+        state: 'locked',
+    },
+    {
+        name: 'stairs-unlocked',
+        item: 'punch bowl',
+        npcs: 'spin-the-bottle',
+        state: 'unlocked',
+    },
+]
---
+++ game/todo.js
@@ -0,0 +1,37 @@
+/* TODO: locations
+    outside *** prologue/car part
+    create bathroom
+    create kitchen
+    create study
+    create stairs
+*/
+
+/* TODO: characters
+    MC's brother
+    bathroom freaks ***** dead dude scene
+    Mr Kitty *** when not commanding you to do actions, randomly offer unimaginable truths
+    2 people blocking stairs
+    spin-the-bottle-fuckers
+ */
+
+/* TODO: puzzles
+    stairs being "locked" *** check for charisma buff
+    bathroom being locked *** check for broken car
+    key to room *** check for poisoned well
+ */
+
+/* TODO: items/action checks
+    coke
+    dead spin-the-bottle fucks
+    key to room
+    car window
+ */
+
+/* TODO: code
+    opening shot/prologue: interval the text so it's like a scrolling type shit
+    make button interface for speech
+    do like zork:
+        KITCHEN
+        blah blah blah
+    
+ */
