9a73146f50ebf7bbf60da33f145fdeb0db8c68e8
Brandon Hall <bran.tyler.hall@gmail.com>
2022-12-06 21:27:42 -0500

    created shell of speech prompts to be used later

---
+++ js/npc-interactions.js
@@ -0,0 +1,89 @@
+const blowFiendSpeech = [
+    // first interaction
+    {
+        id: 1,
+        text: '',
+        options: [
+            // first option
+            {
+                text: '',
+                setState: {
+
+                },
+                nextText: 2
+            }
+        ]
+    }
+];
+
+const stairBlockerSpeech = [
+    // first interaction
+    {
+        id: 1,
+        text: '',
+        options: [
+            // first option
+            {
+                text: '',
+                setState: {
+
+                },
+                nextText: 2
+            }
+        ]
+    }
+];
+
+const stbSpeech = [
+    // first interaction
+    {
+        id: 1,
+        text: '',
+        options: [
+            // first option
+            {
+                text: '',
+                setState: {
+
+                },
+                nextText: 2
+            }
+        ]
+    }
+];
+
+const mrKittyKnowledge = [
+    // first interaction
+    {
+        id: 1,
+        text: '',
+        options: [
+            // first option
+            {
+                text: '',
+                setState: {
+
+                },
+                nextText: 2
+            }
+        ]
+    }
+];
+
+const brotherSpeech = [
+    // first interaction
+    {
+        id: 1,
+        text: '',
+        options: [
+            // first option
+            {
+                text: '',
+                setState: {
+
+                },
+                nextText: 2
+            }
+        ]
+    }
+];
