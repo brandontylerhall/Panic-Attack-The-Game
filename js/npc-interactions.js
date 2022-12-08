02e8988f797de2d6c09fb8ca800c24be20851005
Brandon Hall <bran.tyler.hall@gmail.com>
2022-12-08 15:47:32 -0500

    created shell for speech lists but unsure how to properly implement at this time

--- js/npc-interactions.js
+++ js/npc-interactions.js
@@ -1,89 +1,90 @@
-const blowFiendSpeech = [
+const textNodes = [
     // first interaction
     {
         id: 1,
-        text: '',
+        text: 'Hello, stinkfiend',
         options: [
             // first option
             {
-                text: '',
-                setState: {
-
-                },
+                text: 'boop',
                 nextText: 2
-            }
+            },
+            {
+                text: 'leave',
+                nextText: 2
+            },
         ]
     }
 ];
 
-const stairBlockerSpeech = [
-    // first interaction
-    {
-        id: 1,
-        text: '',
-        options: [
-            // first option
-            {
-                text: '',
-                setState: {
-
-                },
-                nextText: 2
-            }
-        ]
-    }
-];
-
-const stbSpeech = [
-    // first interaction
-    {
-        id: 1,
-        text: '',
-        options: [
-            // first option
-            {
-                text: '',
-                setState: {
-
-                },
-                nextText: 2
-            }
-        ]
-    }
-];
-
-const mrKittyKnowledge = [
-    // first interaction
-    {
-        id: 1,
-        text: '',
-        options: [
-            // first option
-            {
-                text: '',
-                setState: {
-
-                },
-                nextText: 2
-            }
-        ]
-    }
-];
-
-const brotherSpeech = [
-    // first interaction
-    {
-        id: 1,
-        text: '',
-        options: [
-            // first option
-            {
-                text: '',
-                setState: {
-
-                },
-                nextText: 2
-            }
-        ]
-    }
-];
+// const blowFiendSpeech = [
+//     // first interaction
+//     {
+//         id: 1,
+//         text: '',
+//         options: [
+//             // first option
+//             {
+//                 text: '',
+//                 setState: {
+//
+//                 },
+//                 nextText: 2
+//             }
+//         ]
+//     }
+// ];
+//
+// const stairBlockerSpeech = [
+//     // first interaction
+//     {
+//         id: 1,
+//         text: '',
+//         options: [
+//             // first option
+//             {
+//                 text: '',
+//                 setState: {
+//
+//                 },
+//                 nextText: 2
+//             }
+//         ]
+//     }
+// ];
+//
+// const stbSpeech = [
+//     // first interaction
+//     {
+//         id: 1,
+//         text: '',
+//         options: [
+//             // first option
+//             {
+//                 text: '',
+//                 setState: {
+//
+//                 },
+//                 nextText: 2
+//             }
+//         ]
+//     }
+// ];
+//
+// const brotherSpeech = [
+//     // first interaction
+//     {
+//         id: 1,
+//         text: '',
+//         options: [
+//             // first option
+//             {
+//                 text: '',
+//                 setState: {
+//
+//                 },
+//                 nextText: 2
+//             }
+//         ]
+//     }
+// ];
